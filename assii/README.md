f8ef3160cb32d4aa552b8f983104eb417551d778
saja Al-qhtani <saja-alqhtani@sajas-MacBook-Air.local>
2021-09-27 12:11:08 +0300

    MyProject

---
+++ assii/README.md
@@ -0,0 +1,13 @@
+# Week1 Day1 Lab2: Your Name
+
+## Topics
+1. Xcode setup 
+
+## Description
+1. Setup Xcode on your Mac
+2. Create a new project
+3. Display your name on the screen
+4. Upload to this repo as a PR
+
+## DeadLine 
+Sunday 26 September
---
+++ assii/assii/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assii
+//
+//  Created by saja Al-qhtani on 20/02/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
